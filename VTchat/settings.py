f5822786e7c486c9f21d6dcdfd4f9320b2a65a76
KNagaVivek <karnatinagavivek@gmail.com>
2023-08-03 14:47:17 +0530

    s3 added

--- VTchat/settings.py
+++ VTchat/settings.py
@@ -158,3 +158,14 @@ CHANNEL_LAYERS = {
             },
         },
     }
+
+
+AWS_ACCESS_KEY_ID = 'AKIAXDVQDLIZTJHCYMNT '
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'vtchatbucket'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'ap-southeast-2'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
