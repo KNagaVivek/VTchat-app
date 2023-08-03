1370c65be26fb8e464721f67cf72510aaae19e8b
KNagaVivek <karnatinagavivek@gmail.com>
2023-08-03 13:19:42 +0530

    updated code

--- VTchat/settings.py
+++ VTchat/settings.py
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'VTchat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vtchatdatabase',
+        'USER': 'KNVivek',
+        'PASSWORD': 'KNVivek#123',
+        'HOST': 'vtchatdatabase.ccdvtbas5ome.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 
 # Password validation
