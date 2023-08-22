535edbfa586881957927bf514ccaa210984e6bd5
KNagaVivek <karnatinagavivek@gmail.com>
2023-08-22 13:27:43 +0530

    added procfile

--- VTchat/settings.py
+++ VTchat/settings.py
@@ -155,7 +155,7 @@ CHANNEL_LAYERS = {
         "default": {
             "BACKEND": "channels_redis.core.RedisChannelLayer",
             "CONFIG": {
-                "hosts": [("localhost", 6379)],
+                "hosts": [(config('CHANNEL_LAYERS_HOST'), 6379)],
             },
         },
     }
